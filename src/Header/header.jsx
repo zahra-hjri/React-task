b33fff7d292f0cd063d4be47826130e4443932d6
zahra <zhojabri@ymail.com>
2024-08-08 10:38:23 +0330

    icon add

--- src/Header/header.jsx
+++ src/Header/header.jsx
@@ -1,9 +1,12 @@
 import "./header.css";
+import { GiFullPizza } from "react-icons/gi";
 
 const Header = () => {
   return (
-    <header className=" h-[170px] bg-gray-800 text-white">
-      <h2 className="text-white text-end p-16 font-bold tracking-[5px] text-[24px]">
+    <header className=" h-[170px] bg-gray-800 text-white flex justify-around items-center">
+      <GiFullPizza className="h-32 w-32 text-orange-600" />
+
+      <h2 className="text-white text-end font-bold tracking-[5px] text-[24px]">
         Fast Food Catalogue
       </h2>
     </header>
