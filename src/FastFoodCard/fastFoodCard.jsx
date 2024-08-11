bb59d76510e5f3427a5bf5ba25e48ecd38053272
zahra <zhojabri@ymail.com>
2024-08-11 17:23:59 +0330

    stile fastfood list

--- src/FastFoodCard/fastFoodCard.jsx
+++ src/FastFoodCard/fastFoodCard.jsx
@@ -2,19 +2,21 @@ import { BiSolidShoppingBagAlt } from "react-icons/bi";
 
 const FastFoodCard = ({ id, imageUrl, name, ingredients, price }) => {
   return (
-    <div className="w-full relative flex-col p-4 shadow-lg rounded-lg" key={id}>
+    <div className="w-full relative flex-col shadow-lg rounded-md" key={id}>
       <div className="">
         <span className="bg-green-500 absolute top-6 right-6 text-white text-xs font-medium me-2 px-4 py-2">
           {price}
         </span>
-        <img className="rounded-lg" src={imageUrl} alt={name} />
+        <img className="rounded-md" src={imageUrl} alt={name} />
       </div>
-      <strong className="text-[12px] block mt-4">{name}</strong>
-      <p className="text-[12px] my-3">{ingredients}</p>
-      <button className="w-full h-10 bg-orange-600 rounded-lg text-white py-0.5 flex gap-3 justify-center items-center m-auto">
-        <span className="text-[12px]"> افزودن به سبد خرید</span>
-        <BiSolidShoppingBagAlt className="text-lg"></BiSolidShoppingBagAlt>
-      </button>
+      <div className="flex-col justify-center items-center gap-2 text-center">
+        <strong className="text-[12px] block">{name}</strong>
+        <p className="text-[12px]">{ingredients}</p>
+        <button className="w-full h-10 bg-orange-600 rounded-lg text-white py-0.5 flex gap-3 justify-center items-center m-auto">
+          <span className="text-[12px]"> افزودن به سبد خرید</span>
+          <BiSolidShoppingBagAlt className="text-lg"></BiSolidShoppingBagAlt>
+        </button>
+      </div>
     </div>
   );
 };
