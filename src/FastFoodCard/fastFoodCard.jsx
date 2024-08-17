af0b0fec14ecc93ed139ad0c3cdc85fd48d3e0ab
zahra <zhojabri@ymail.com>
2024-08-17 17:51:57 +0330

    palceholder image

--- src/FastFoodCard/fastFoodCard.jsx
+++ src/FastFoodCard/fastFoodCard.jsx
@@ -1,19 +1,22 @@
 import { BiSolidShoppingBagAlt } from "react-icons/bi";
+import "./FastFoodCard.css";
 
 const FastFoodCard = ({ imageUrl, name, ingredients, price }) => {
   return (
-    <div className="w-full relative flex-col shadow-lg rounded-md">
+    <div className="card w-full relative flex-col shadow-2xl">
       <div className="">
-        <span className="bg-green-500 flex items-center justify-center absolute top-4 right-4 text-white text-xs font-medium me-2 px-4 py-0.5 rounded-sm">
+        <span className="bg-green-500 flex items-center justify-center absolute top-4 right-4 text-white text-xs font-medium me-2 px-4 py-0.5 ">
           قیمت: {price.toLocaleString()} تومان
         </span>
-        <img className="rounded-md w-full h-full" src={imageUrl} alt={name} />
+        <div className="card__img">
+          <img className="w-full h-full" src={imageUrl} alt={name} />
+        </div>
       </div>
       <div className="flex flex-col py-4 text-center ">
         <strong className="text-[12px] block mb-3">{name}</strong>
         <p className="text-[10px]  mb-4 h-[40px]">{ingredients}</p>
 
-        <button className="w-[90%] h-8 bg-transparent border-green-400 hover:bg-green-400 text-green-400 hover:text-white border-[1px] rounded-md py-0.5 flex gap-3 justify-center items-center mx-auto">
+        <button className="w-[90%] h-8 bg-transparent border-green-400 hover:bg-green-400 text-green-400 hover:text-white border-[1px]  py-0.5 flex gap-3 justify-center items-center mx-auto">
           <span className="text-[12px] "> افزودن به سبد خرید</span>
           <BiSolidShoppingBagAlt className="text-lg "></BiSolidShoppingBagAlt>
         </button>
