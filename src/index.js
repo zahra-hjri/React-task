ad80a39a9c94d1c394b840a74a23ca9328297833
zahra <zhojabri@ymail.com>
2024-09-17 10:39:00 +0330

    clean app

--- src/index.js
+++ src/index.js
@@ -2,16 +2,14 @@ import React, { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import "./index.css";
-// import App from "./App";
+import App from "./App";
 
-import { store } from "./store";
-import App2 from "./App2";
 import "./fonts/IRANSansWeb.ttf";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <StrictMode>
-    <Provider store={store}>
-      <App2 />
+    <Provider>
+      <App />
     </Provider>
   </StrictMode>
 );
