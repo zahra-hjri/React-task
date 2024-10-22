012975283c789393978975b4253c4c3d37d917d1
zahra <zhojabri@ymail.com>
2024-10-22 09:38:07 +0330

    deleted fonts

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import React, { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
-// import { Provider } from "react-redux";
 import "./index.css";
 import App from "./App";
 
-import "./fonts/IRANSansWeb.ttf";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <StrictMode>
